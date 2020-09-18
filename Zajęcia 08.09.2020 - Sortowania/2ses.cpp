39ca78b35fcc1ca55010b868f67fdf666ae87223
Dorian Gabler <61635324+dorian-gabIer@users.noreply.github.com>
2020-09-18 08:37:37 +0200

    Update 2ses.cpp

--- Zajęcia 08.09.2020 - Sortowania/2ses.cpp
+++ Zajęcia 08.09.2020 - Sortowania/2ses.cpp
@@ -6,10 +6,16 @@ void sortowanie(int a[], int N)
     for (int i = 0; i < N-1; i++)  
     {  
         minpos = i;  
-        for (int k = i+1; k < N; k++) if (a[k] < a[minpos]) minpos = k;
-        int temp = a[minpos];  
-        a[minpos] = a[i];  
-        a[i] = temp;
+        for (int k = i+1; k < N; k++) 
+        {
+            if (a[k] < a[minpos])
+            {
+                minpos = k;
+                int temp = a[minpos];  
+                a[minpos] = a[i];  
+                a[i] = temp;
+            }
+        }
         for (int j = 0; j < N; j++) cout << a[j] << " ";
         cout << endl;
     }
