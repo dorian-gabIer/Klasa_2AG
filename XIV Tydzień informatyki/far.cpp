946edcbc8aded19ad01d0a685ae36a77d5ef4e0e
Dorian Gabler <61635324+dorian-gabIer@users.noreply.github.com>
2020-10-12 18:07:56 +0200

    Update far.cpp

--- XIV Tydzień informatyki/far.cpp
+++ XIV Tydzień informatyki/far.cpp
@@ -1,20 +1,17 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
     int n, sum = 0, p, q, r;
     cin >> n;
-    int tab[n] = {};
-    while(1)
+    int tab[10005] = { 0 };
+    do
     {
         cin >> p >> q >> r;
-        if(p == 0 && q == 0 && r == 0) break;
-        for(int i = p-1; i < q; i++) if(tab[i] < r) tab[i] = r;
-
-    }
-    for(int i = 0; i < n; i++)
+        for (int i = p-1; i < q; i++) if (tab[i] < r) tab[i] = r;
+    } 
+    while (p && q && r);
+    for (int i = 0; i < n; i++)
     {
         sum += tab[i];
     }
