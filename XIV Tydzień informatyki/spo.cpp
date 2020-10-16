c9bf6a9acb2b1a494119c635488d3fa11a008b85
Dorian Gabler <61635324+dorian-gabIer@users.noreply.github.com>
2020-10-16 13:35:58 +0200

    Update spo.cpp

--- XIV Tydzień informatyki/spo.cpp
+++ XIV Tydzień informatyki/spo.cpp
@@ -4,20 +4,19 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int n, count = 0, max = 0;
+    int n, count = 0, maxi = 0;
     cin >> n;
     int tab[n];
     for(int i = 0; i < n; i++) cin >> tab[i];
-    sort(tab, tab+n);
     for(int i = 0; i < n-1; i++)
     {
         if(tab[i] == tab[i+1] || tab[i+1] - tab[i] == 1) count++;
         else
         {
-            if(max < count) max = count;
+            if(maxi < count) maxi = count;
             count = 0;
         }
     }
-    cout << max+1;
+    cout << max(maxi, count) + 1;
     return 0;
 }
