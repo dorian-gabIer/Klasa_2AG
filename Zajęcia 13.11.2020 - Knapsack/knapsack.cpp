6fd515df7323f046dc871cfa664b4ccb11192f75
Dorian Gabler <61635324+dorian-gabIer@users.noreply.github.com>
2020-11-19 21:45:45 +0100

    Update knapsack.cpp

--- Zajęcia 13.11.2020 - Knapsack/knapsack.cpp
+++ Zajęcia 13.11.2020 - Knapsack/knapsack.cpp
@@ -1,27 +1,21 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
 using namespace std;
-int knapsack(int *priceold, int *weightold, int n, int v, bool elim)
+int knapsack(int* price, int* weight, int n, int v, bool elim)
 {
-    int price[100], weight[n], totalweight = 0, max1, max2, worth = 0;
-    double max;
-    for(int i = 0; i < n; i++)
-    {
-        max1 = -1, max2 = -1, max = -1;
-        for(int j = i; j < n; j++) if(priceold[j]/double(weightold[j]) > max) max = priceold[j]/weightold[j], max1 = priceold[j], max2 = weightold[j];
-        price[i] = max1;
-        weight[i] = max2;
-    }
+    int totalweight = 0, worth = 0;
+    for (int i = n - 1; i >= 0; i--) for (int j = 0; j < i; j++) if ((price[j] / double(weight[j])) < (price[j + 1] / double(weight[j + 1]))) swap(price[j], price[j + 1]), swap(weight[j], weight[j + 1]);
     // dla elim: algorytm decyzyjny
     // dla !elim: algorytm ogolny
-    if(!elim) for(int i = 0; i < n; i++) while(totalweight + weight[i] <= v) worth += price[i], totalweight += weight[i];
-    else for(int i = 0; i < n; i++) if(totalweight + weight[i] <= v) worth += price[i], totalweight += weight[i];
+    if (!elim) for (int i = 0; i < n; i++) while (totalweight + weight[i] <= v) worth += price[i], totalweight += weight[i];
+    else for (int i = 0; i < n; i++) if (totalweight + weight[i] <= v) worth += price[i], totalweight += weight[i];
     return worth;
 }
 int main()
 {
     int n = 6, v = 15;
-    int price[100] = {6, 4, 5, 7, 10, 2};
-    int weight[100] = {6, 2, 3, 2, 3, 1};
+    int price[100] = { 6, 4, 5, 7, 10, 2 };
+    int weight[100] = { 6, 2, 3, 2, 3, 1 };
     cout << knapsack(price, weight, n, v, false) << endl << knapsack(price, weight, n, v, true);
     return 0;
 }
